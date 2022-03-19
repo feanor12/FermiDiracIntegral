e2fc9592f460a927529dd1d90811782736114bcb
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2022-03-19 20:03:08 +0100

    Add source for the approximation

--- src/FermiDiracIntegral.jl
+++ src/FermiDiracIntegral.jl
@@ -27,6 +27,9 @@ Approximation of the complete Fermi-Dirac-integral for j = 1/2
 Checked for a relative tolerance of 3% in the range x = -100:0.1:100
 
 Speed: 100 times faster than the polylog version
+
+Source:
+J. S. Blakemore: Approximations for Fermi-Dirac Integrals. Solid-State Electronics, 25(11):1067-1076, 1982.
 """
 function F(::Val{1/2},x) 
     if x < 1.3
